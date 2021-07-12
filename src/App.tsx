c55d3a52b8cde87e95f422fffec35f788304edfb
AmitMahato643 <amit643.mahato@gmail.com>
2021-07-12 19:03:48 +0545

    use react-stripe-checkout for card details UI & requests handling with stripe server

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StripeCheckout, { Token } from "react-stripe-checkout";
 
 function App() {
+  const [product, setProduct] = useState<{
+    name: string;
+    price: number;
+    provider: string;
+  }>({
+    name: "Complete Course On React",
+    price: 200,
+    provider: "facebook",
+  });
+
+  const makePayment = (token: Token) => {
+    const body = {
+      token,
+      product,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return fetch(process.env.REACT_APP_SERVER_HOST + "/payment", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((res) => {
+        console.log("Response : ", res);
+        const { status } = res;
+        console.log("Status : ", status);
+      })
+      .catch((err) => console.log("Error : ", err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3000#"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <StripeCheckout
+          stripeKey={process.env.REACT_APP_KEY || ""}
+          token={makePayment}
+          name={`Buy ${product.name}`}
+          amount={product.price * 100}
+          shippingAddress
+          billingAddress
+        >
+          <button className="btn-large">
+            Buy {product.name} at just {product.price}$
+          </button>
+        </StripeCheckout>
       </header>
     </div>
   );
